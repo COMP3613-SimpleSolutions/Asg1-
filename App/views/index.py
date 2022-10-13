b53b4671d131f00942f16866053f7c009d4489f0
Kristian-480 <krisrichards10@hotmail.com>
2022-10-13 20:37:55 +0000

    make test

--- App/views/index.py
+++ App/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -6,11 +6,11 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
-@index_views.route('/make', methods=['POST'])
+@index_views.route('/make', methods=['GET'])
 def makeRecom_action():
     data = request.json
-    newrec = Recommendation(title=data['recomTitle'], description=data['recomDesc'], course=data['course'], comments=None, status="unchecked")
-
+    #newrec = Recommendation(title=data['recomTitle'], description=data['recomDesc'], course=data['course'], comments=None, status="unchecked")
+    return jsonify({"message": "hellow"})
     #db.session.add(newrec)
     #db.session.commit()
-    return jsonify({"message":f" {data['recomTitle']} recommendation created "})
+    #return jsonify({"message":f" {data['recomTitle']} recommendation created "})
