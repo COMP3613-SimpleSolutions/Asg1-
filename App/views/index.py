e8d1307257e0c3cc8a196cebc18cd3c69fb7536b
Kristian-480 <krisrichards10@hotmail.com>
2022-10-13 20:12:00 +0000

    first feat ttest

--- App/views/index.py
+++ App/views/index.py
@@ -5,3 +5,12 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 @index_views.route('/', methods=['GET'])
 def index_page():
     return render_template('index.html')
+
+@index_views.route('/make', methods=['POST'])
+def makeRecom_action():
+    data = request.json
+    newrec = Recommendation(title=data['recomTitle'], description=data['recomDesc'], course=data['course'], comments=None, status="unchecked")
+
+    db.session.add(newrec)
+    db.session.commit()
+    return jsonify({"message":f" {data['recomTitle']} recommendation created "})
