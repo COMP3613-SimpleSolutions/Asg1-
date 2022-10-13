0de66f23d16dc38d940425b7440ab139f15673e3
Kristian-480 <krisrichards10@hotmail.com>
2022-10-13 20:54:13 +0000

    recom to views

--- App/views/index.py
+++ App/views/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.models import User, Staff, Student
+from App.models import User, Staff, Student, Recommendation
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
