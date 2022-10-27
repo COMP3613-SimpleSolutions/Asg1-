15d2382a111534931de50b134cad24348734a1f2
Kristian-480 <krisrichards10@hotmail.com>
2022-10-27 21:20:05 +0000

    Readded tests

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -21,4 +21,25 @@ LOGGER = logging.getLogger(__name__)
 '''
    Unit Tests
 '''
+class UserUnitTests(unittest.TestCase):
 
+    def test_new_user(self):
+        user = User("bob", "bobpass")
+        assert user.username == "bob"
+
+    # pure function no side effects or integrations called
+    def test_toJSON(self):
+        user = User("bob", "bobpass")
+        user_json = user.toJSON()
+        self.assertDictEqual(user_json, {"id":None, "username":"bob"})
+    
+    def test_hashed_password(self):
+        password = "mypass"
+        hashed = generate_password_hash(password, method='sha256')
+        user = User("bob", password)
+        assert user.password != password
+
+    def test_check_password(self):
+        password = "mypass"
+        user = User("bob", password)
+        assert user.check_password(password)
